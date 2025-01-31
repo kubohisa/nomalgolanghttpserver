a7bae8f8c60b03fce88ee5cf108c2c4cc467ec94
kubohisa <kubohisa@gmail.com>
2025-01-31 14:48:51 +0900

    Add files via upload

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -16,5 +17,5 @@ func main() {
 
 	fmt.Print("Listening.")
 	
-	server.ListenAndServe()
+	log.Fatal(server.ListenAndServe())
 }
